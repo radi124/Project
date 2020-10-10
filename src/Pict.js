00cbc4ad3928aad3bf261d36c71795db73f8b506
Radosław Mikuta <radek.mikuta11@onet.pl>
2020-10-10 23:10:46 +0200

    Picture added

--- src/Pict.js
+++ src/Pict.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import "./Pict.css";
 export const Pict = (props) => {
-  return <div id="thatIsFirstPhoto">{props.zdjecie}</div>;
+  return (
+    <div id="BPict">
+      <div id="thatIsFirstPhoto">{props.zdjecie}</div>
+    </div>
+  );
 };
