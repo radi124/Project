52b0364131c5211e85cde249eab150aa3901a597
Radosław Mikuta <radek.mikuta11@onet.pl>
2020-11-16 23:37:01 +0100

    repair

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,173 +1,96 @@
 import React from "react";
 import styled from "styled-components";
 import { Grid, Row, Col } from "react-flexbox-grid";
-import photo1 from "../images/phone.svg";
-import FacebookLogo from "../images/facebook.svg";
-import EmailLogo from "../images/email.svg";
-import Instalogo from "../images/instagram.svg";
+import phoneIcon from "../images/phone.svg";
+import facebookLogo from "../images/facebook.svg";
+import emailLogo from "../images/email.svg";
+import instalogo from "../images/instagram.svg";
 
 export const Footer = () => {
   return (
-    <NavigationWrapper id="xd">
+    <NavigationWrapper>
       <Grid>
-        <Container>
-          <NavigationWrapper2>
-            <Row>
-              <Col>
-                <Line>
-                  <H2Wrapper>
-                    Myczkowce
-                    <div>Sadyba</div>
-                  </H2Wrapper>
-                  Myczkowce 82 38-623 Solina ,Polska
-                </Line>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Border></Border>
-              </Col>
-            </Row>
-
-            <Row center="xs">
-              <Col xs={12}>
-                <Row>
-                  <Col xs={6} sm={4} md={4} lg={4}>
-                    <CopyRightWrapper>
-                      Copyright © 2018-2020 Sadyba Sp. z o.o. All rights
-                      reserved.
-                    </CopyRightWrapper>
-                  </Col>
-                  <Col xs={6} sm={4} md={4} lg={4}>
-                    <PhotoWrapper>
-                      <Photo src={photo1} />
-                      609 470 676
-                    </PhotoWrapper>
-                  </Col>
-
-                  <Col xs={12} sm={4} md={4} lg={4}>
-                    <IconsWrapper>
-                      <FbWrapper>
-                        {" "}
-                        <a
-                          href="https://www.facebook.com/myczkowcesadyba/?epa=SEARCH_BOX"
-                          target="_blank"
-                        >
-                          <img src={FacebookLogo} />
-                        </a>
-                      </FbWrapper>
-                      <EMailWrapper>
-                        <a
-                          href=" https://www.instagram.com/explore/locations/226554641416824/myczkowce-sadyba/?hl=pl  "
-                          target="_blank"
-                        >
-                          <img src={EmailLogo} />
-                        </a>
-                      </EMailWrapper>
-                      <InstaWrapper>
-                        <a
-                          href=" https://www.instagram.com/myczkowce_sadyba/?hl=pl"
-                          target="_blank"
-                        >
-                          <img src={Instalogo} />
-                        </a>
-                      </InstaWrapper>
-                    </IconsWrapper>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </NavigationWrapper2>
-        </Container>
+        <Row center="xs">
+          <Col>
+            <H2Wrapper>
+              Myczkowce
+              <br />
+              Sadyba
+            </H2Wrapper>
+            Myczkowce 82 38-623 Solina ,Polska
+          </Col>
+        </Row>
+      </Grid>
+      <Hr />
+      <Grid>
+        <Row center="xs">
+          <Col xs={6} sm={4} md={4} lg={4}>
+            Copyright © 2018-2020 Sadyba Sp. z o.o. All rights reserved.
+          </Col>
+          <Col xs={6} sm={4} md={4} lg={4}>
+            <PhoneWrapper href="tel:609-470-676">
+              <Photo src={phoneIcon} />
+              609 470 676
+            </PhoneWrapper>
+          </Col>
+          <Col xs={12} sm={4} md={4} lg={4}>
+            <IconsWrapper>
+              <Link
+                href="https://www.facebook.com/myczkowcesadyba/?epa=SEARCH_BOX"
+                target="_blank"
+              >
+                <Photo src={facebookLogo} />
+              </Link>
+              <Link
+                href=" https://www.instagram.com/explore/locations/226554641416824/myczkowce-sadyba/?hl=pl  "
+                target="_blank"
+              >
+                <Photo src={emailLogo} />
+              </Link>
+              <Link
+                href=" https://www.instagram.com/myczkowce_sadyba/?hl=pl"
+                target="_blank"
+              >
+                <Photo src={instalogo} />
+              </Link>
+            </IconsWrapper>
+          </Col>
+        </Row>
       </Grid>
     </NavigationWrapper>
   );
 };
 
+const Link = styled.a`
+  margin: 0px 5px;
+`;
+const Hr = styled.hr`
+  margin-bottom: 30px;
+`;
 const NavigationWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  height: 300px;
-  width: 100%;
+  padding: 20px 0px 40px 0px;
   background-color: rgba(0, 0, 0, 0.6);
-`;
-const NavigationWrapper2 = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  a {
-    text-decoration: none;
-    color: rgb(255, 255, 255);
-  }
-`;
-const Line = styled.div`
-  align-items: center;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
 `;
 const H2Wrapper = styled.h2`
   color: rgb(255, 255, 255);
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
 `;
-const Border = styled.tr`
-  border-bottom: 1px solid grey;
-  width: 800px;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100vw;
-`;
-
 const Photo = styled.img`
   width: 30px;
 `;
-const PhotoWrapper = styled.div`
+const PhoneWrapper = styled.a`
   align-items: center;
   display: flex;
   flex-direction: row;
   justify-content: center;
-  padding-top: 20px;
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+  :hover {
+    cursor: pointer;
+  }
 `;
-
-const FbWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-right: 10px;
-`;
-const EMailWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-right: 10px;
-`;
-const InstaWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-right: 10px;
-`;
-
 const IconsWrapper = styled.div`
   align-items: center;
   display: flex;
   flex-direction: row;
   justify-content: center;
-  padding-top: 20px;
 `;
-const CopyRightWrapper = styled.div`
-  padding-top: 20px;
-`;
-const Container = styled.div``;
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -13,70 +13,55 @@ export const Main = (props) => {
         <Image src={photo4} />
       </ImageWrapper>
       <Grid>
-        <Container>
+        <Row center="xs">
+          <Col
+            xs={12}
+            id="S2Container"
+            md={10}
+            mdofset={1}
+            sm={10}
+            smofset={1}
+            lg={8}
+            lgofset={2}
+          >
+            <H1Wrapper> {props.Drugazakładka}</H1Wrapper>
+          </Col>
+
+          <Col>
+            <FirstTextWrapper>
+              Obiekt MYCZKOWCE SADYBA położony jest w miejscowości Uherce
+              Mineralne. Wszystkie domy obejmują salon z sofą oraz w pełni
+              wyposażoną kuchnię. W łazience znajduje się wanna lub prysznic.
+              Obiekt MYCZKOWCE SADYBA jest oddalony o 13 km od Polańczyka i o 38
+              km od Arłamowa. Obiekt MYCZKOWCE SADYBA położony jest w
+              miejscowości Uherce Mineralne. Wszystkie domy obejmują salon z
+              sofą oraz w pełni wyposażoną kuchnię. W łazience znajduje się
+              wanna lub prysznic. Obiekt MYCZKOWCE SADYBA jest oddalony o 13 km
+              od Polańczyka i o 38 km od Arłamowa. Obiekt MYCZKOWCE SADYBA
+              położony jest w miejscowości Uherce Mineralne. Wszystkie domy
+              obejmują salon z sofą oraz w pełni wyposażoną kuchnię. W łazience
+              znajduje się wanna lub prysznic. Obiekt MYCZKOWCE SADYBA jest
+              oddalony o 13 km od Polańczyka i o 38 km od Arłamowa.
+            </FirstTextWrapper>
+
+            <InfoButtonWrapper>
+              <InfoButton>
+                {" "}
+                <a href="../Restauracja"> więcej</a>
+              </InfoButton>
+            </InfoButtonWrapper>
+          </Col>
+        </Row>
+        <Restauracja>
           <Row center="xs">
-            <Col xs={12} id="S2Container">
-              <H2Wrapper> {props.Drugazakładka}</H2Wrapper>
+            <Col xs={12} sm={5} md={4} lg={5}>
+              <PhotoColumn src={photo1} />
             </Col>
-            <InfoWrapper>
-              <Col>
-                Obiekt MYCZKOWCE SADYBA położony jest w miejscowości Uherce
-                Mineralne.
-                <div>
-                  Wszystkie domy obejmują salon z sofą oraz w pełni wyposażoną
-                  kuchnię. W łazience znajduje się wanna lub prysznic.
-                </div>
-                <div>
-                  Obiekt MYCZKOWCE SADYBA jest oddalony o 13 km od Polańczyka i
-                  o 38 km od Arłamowa.
-                </div>
-                <InfoButtonWrapper>
-                  <InfoButton>
-                    {" "}
-                    <a href="../Restauracja"> więcej</a>
-                  </InfoButton>
-                </InfoButtonWrapper>
-              </Col>
-            </InfoWrapper>
-          </Row>
-          <Restauracja>
-            <Row center="xs">
-              <Col xs={12} sm={5} md={4} lg={5}>
-                <PhotoColumn src={photo1} />
-              </Col>
-              <Col xs={12} sm={7} md={4} lg={7}>
-                <RestauracjaWrapper>
-                  <H2Wrapper>
-                    <a>Restauracja</a>
-                  </H2Wrapper>
-                  <TextWrapper>
-                    Lorem ipsum dolor sit amet enim. Etiam ullamcorper.
-                    Suspendisse a pellentesque dui, non felis. Maecenas
-                    malesuada elit lectus felis, malesuada ultricies. Curabitur
-                    et ligula. Ut molestie a, ultricies
-                    <div>
-                      porta urna. Vestibulum commodo volutpat a, Lorem ipsum
-                      dolor sit amet enim. Etiam ullamcorper. Slutpat a,
-                    </div>
-                  </TextWrapper>
-                  <InfoButtonWrapper>
-                    <InfoButton>
-                      <a href="../Restauracja">więcej</a>
-                    </InfoButton>
-                  </InfoButtonWrapper>
-                </RestauracjaWrapper>
-              </Col>
-            </Row>
-            <Row center="xs">
-              <Col xs={6} id="S2Container"></Col>
-            </Row>
-          </Restauracja>
-          <Domki>
-            <Row center="xs">
-              <Col xs={12} sm={7} md={4} lg={7}>
-                <H2Wrapper>
-                  <a>Domki</a>
-                </H2Wrapper>
+            <Col xs={12} sm={7} md={4} lg={7}>
+              <RestauracjaWrapper>
+                <H1Wrapper>
+                  <a>Restauracja</a>
+                </H1Wrapper>
                 <TextWrapper>
                   Lorem ipsum dolor sit amet enim. Etiam ullamcorper.
                   Suspendisse a pellentesque dui, non felis. Maecenas malesuada
@@ -84,22 +69,49 @@ export const Main = (props) => {
                   Ut molestie a, ultricies
                   <div>
                     porta urna. Vestibulum commodo volutpat a, Lorem ipsum dolor
-                    sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque
-                    dui, non felis urna. Vestibulum commodo volutpat a,
+                    sit amet enim. Etiam ullamcorper. Slutpat a,
                   </div>
                 </TextWrapper>
                 <InfoButtonWrapper>
                   <InfoButton>
-                    <a href="../Domki"> więcej</a>
+                    <a href="../Restauracja">więcej</a>
                   </InfoButton>
                 </InfoButtonWrapper>
-              </Col>
-              <Col xs={12} sm={5} md={4} lg={5}>
-                <PhotoColumn src={photo2} />
-              </Col>
-            </Row>
-          </Domki>
-        </Container>
+              </RestauracjaWrapper>
+            </Col>
+          </Row>
+          <Row center="xs">
+            <Col xs={6} id="S2Container"></Col>
+          </Row>
+        </Restauracja>
+        <Domki>
+          <Row center="xs">
+            <Col xs={12} sm={7} md={4} lg={7}>
+              <H1Wrapper>
+                <a>Domki</a>
+              </H1Wrapper>
+              <TextWrapper>
+                Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse
+                a pellentesque dui, non felis. Maecenas malesuada elit lectus
+                felis, malesuada ultricies. Curabitur et ligula. Ut molestie a,
+                ultricies
+                <div>
+                  porta urna. Vestibulum commodo volutpat a, Lorem ipsum dolor
+                  sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque
+                  dui, non felis urna. Vestibulum commodo volutpat a,
+                </div>
+              </TextWrapper>
+              <InfoButtonWrapper>
+                <InfoButton>
+                  <a href="../Domki"> więcej</a>
+                </InfoButton>
+              </InfoButtonWrapper>
+            </Col>
+            <Col xs={12} sm={5} md={4} lg={5}>
+              <PhotoColumn src={photo2} />
+            </Col>
+          </Row>
+        </Domki>
       </Grid>
     </>
   );
@@ -118,21 +130,22 @@ const PhotoColumn = (props) => {
 const Photo = styled.img`
   width: 100%;
   height: 230px;
-  padding-top: 14px;
+  margin-top: 14px;
   object-fit: cover;
   border-radius: 12px;
-  box-shadow: 0 0px 6px rgba(0, 0, 0, 0.12), 0 0px 6px rgba(0, 0, 0, 0.12);
+  box-shadow: 0 0px 6px rgba (119 119 119 0.26),
+    0 0px 6px rgba (119 119 119 0.26);
   transition: 0.5s;
 
   :hover {
-    box-shadow: 0 0px 12px rgba(0, 0, 0, 0.24), 0 0px 12px rgba(0, 0, 0, 0.24);
+    box-shadow: 0 0px 12px rgba (119 119 119 0.26),
+      0 0px 12px rgba (119 119 119 0.26);
     cursor: pointer;
   }
 `;
 
 const PhotoWrapper = styled.div`
   width: 100%;
-  height: 200px;
 `;
 
 const Image = styled.img`
@@ -141,12 +154,11 @@ const Image = styled.img`
   object-fit: cover;
 `;
 const ImageWrapper = styled.div``;
-const InfoWrapper = styled.a`
-  height: 170px;
-`;
 const InfoButton = styled.button`
-  padding: 7px;
+  padding:10px 16px;
   border-radius: 10px;
+  font-weight:bold;
+  font-size:14px;
   cursor: pointer;
   color: white;
   background-color: rgb(236, 159, 14);
@@ -161,9 +173,8 @@ const InfoButton = styled.button`
   :hover {
     box-shadow: 0 0px 2px  #848484, 0 0px 2px  #848484;
 `;
-const H2Wrapper = styled.h2`
+const H1Wrapper = styled.h1`
   color: #ffff;
-  padding-top: px;
   a {
     color: #ffff;
     text-decoration: none;
@@ -181,11 +192,14 @@ const InfoButtonWrapper = styled.div`
 `;
 const TextWrapper = styled.div`
   text-align: left;
+  margin: 0px 15px 0px 15px;
 `;
 const RestauracjaWrapper = styled.div`
   padding-top: 30px !important;
 `;
-const Container = styled.div`
-  padding-left: 40px;
-  padding-right: 40px;
+
+const FirstTextWrapper = styled.div`
+  text-align: left;
+  padding-left: 10px;
+  padding-right: 10px;
 `;
